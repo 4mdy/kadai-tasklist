b946503c9290b2fd0ebd696fa46dcef3b43b084d
Yusuke Shimoda <shimoda.yuusuke@lmi.ne.jp>
2020-06-19 12:51:11 +0000

    20200619

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,15 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
 
 	def index
-		@tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
+
+
+    #@tasks = Task.all
   end
 
   def show
@@ -13,12 +20,13 @@ class TasksController < ApplicationController
   end
 
   def create
-  	@task = Task.new(task_params)
-
+  	#@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -28,11 +36,11 @@ class TasksController < ApplicationController
   end
 
   def update
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @task = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
