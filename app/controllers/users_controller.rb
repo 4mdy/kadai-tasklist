0ff60603ca153cf86fb67b03187d00b2d4200d85
Yusuke Shimoda <shimoda.yuusuke@lmi.ne.jp>
2020-06-22 07:30:00 +0000

    20200622

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,6 @@ class UsersController < ApplicationController
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
 
   def new
     @user = User.new
