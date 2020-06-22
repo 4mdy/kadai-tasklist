8d675406e3abf4fc3e6ba2766cb9b2d4b84f49d3
Yusuke Shimoda <shimoda.yuusuke@lmi.ne.jp>
2020-06-22 08:37:42 +0000

    06221737

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
 
-  def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-
+  #いらないから消す
+  #def index
+    #@users = User.order(id: :desc).page(params[:page]).per(25)
+  #end
 
   def new
     @user = User.new
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
 	belongs_to :user
+	validates :status, presence: true
+	validates :content, presence: true
 end
